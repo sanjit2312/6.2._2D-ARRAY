6d594738744b7b9fe70fe1045229ba80dfe7650c
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:44:46 +0530

    Add files via upload

---
+++ 5._Array_Length.java
@@ -0,0 +1,15 @@
+package array_2D;
+
+public class Array_Length 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int num[][]=new int[3][4];
+		System.out.println("Array Row Size = "+num.length);
+		System.out.println("Array Column Size = "+num[0].length);
+
+	}
+
+}
