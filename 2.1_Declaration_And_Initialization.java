7a30f8537de8ab633ba3960cc0e127b78f48c940
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:43:19 +0530

    Add files via upload

---
+++ 2.1_Declaration_And_Initialization.java
@@ -0,0 +1,15 @@
+package array_2D;
+
+public class Declaration_And_Initialization 
+{
+
+	public static void main(String[] args)
+	{
+
+		int num[][]= {{11,22,33,44},{55,66,77,88},{10,20,30,40}};
+		System.out.println("num[2][1]= "+num[2][1]);
+		System.out.println("num[0][2]= "+num[0][2]);
+
+	}
+
+}
